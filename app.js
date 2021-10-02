46661753c0c5c7be44f8acf27588c2dc839934c7
ALANA1905 <89952373+ALANA1905@users.noreply.github.com>
2021-10-02 14:35:50 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function CarregarFilmes(){
+    
+            let filmes = ["images/arrival.png" ;"images/escola.png" , "images/yesterday"]
+
+    document.querySelector(".lista-filmes").innerHTML="<img src=" + filmes[0] + ">
+    document.querySelector(".lista-filmes").innerHTML="<img src=" + filmes[0] + ">"
+    document.querySelector(".lista-filmes").innerHTML="<img src=" + filmes[0] + ">"
+    
+
+
+}
