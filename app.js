81ce317ca912a6805be2fdabf81251021f25b802
ALANA1905 <89952373+ALANA1905@users.noreply.github.com>
2021-10-09 21:18:06 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
-function CarregarFilmes(){
-    let filmes = ["images/arrival.png" , "images/yesterday.png" , "images/escola.png" ,"images/eminen.jpg" , "images/toquio.jpg" , "images/nairobi.jpg" ]
+//array --> variável com subdivisões (índices)
+let filmes = [
+    "images/arrival.png" , 
+    "images/eminen.jpg" , 
+    "images/escola.png" , 
+    "images/nairobi.jpg" , 
+    "images/toquio.jpg" , 
+    "images/yesterday.png" , 
+    "images/professor.jpg"
+]
 
-    for(let controle = 0 ; controle < 6 ; controle = controle + 1){
-        document.querySelector(".lista-filmes").innerHTML += "<img src="+ filmes[controle] + ">"
+//refatoração
+//melhorar um código existente.
+
+function CarregarFilmes(){
+    let totalFilmes = filmes.length
+    let divListaFilmes = document.querySelector(".lista-filmes")
+
+    divListaFilmes.innerHTML = ""
+
+    //laço de repetição
+    for(let i = 0 ; i < totalFilmes ; i++){
+        document.querySelector(".lista-filmes").innerHTML += "<img src="+ filmes [i] + ">"
+
     }
 }
+
+function AdicionarFilme(){
+    let nomeFilme = document.querySelector("#nome-do-filme").value
+    let caminhoCompleto = "images/" + nomeFilme
+
+    filmes.push(caminhoCompleto)
+
+    CarregarFilmes()
+} 
